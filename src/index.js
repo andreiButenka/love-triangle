2f1211f8e0df77e7a3a9d9d6edb47684b66ba4a7
andreiButenka <andreiButenka@gmail.com>
2019-03-11 23:51:49 +0300

    feat: add function that counts number of love triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0;  
+  for (let i = 1; i <= preferences.length; i++) {
+    let loveOfFirst = preferences[i - 1];
+    if ( i == loveOfFirst) continue; // checking if he loves himself
+    let loveOfSecond = preferences[loveOfFirst - 1];
+    if (loveOfSecond == i) continue; // checking if lover2 loves lover1 too
+    let loveOfThird = preferences[loveOfSecond - 1];
+    if (loveOfThird == loveOfFirst) continue; // checking if lover3 loves lover2 too 
+    if (loveOfThird == i) { 
+      count += 1;
+    }
+  }
+  return count / 3;
 };
+
